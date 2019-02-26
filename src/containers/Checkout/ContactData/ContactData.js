7abd4c04b8ea5255e9dd03f4b47e9e77910419a5
Colin Sergay <Colin.Sergay@flarehr.com>
2019-02-26 12:38:07 +1100

    Added input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
+        orderForm: {            
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },  
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Street'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },    
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },        
         loading: false
     }
 
@@ -21,16 +60,7 @@ class ContactData extends Component {
         this.setState({loading:true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Colin Sergay',
-                address: {
-                    street: '1 Test Road',
-                    country: 'Australia'
-                },
-                email: 'colin@test.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price            
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -43,13 +73,19 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({id: key, config: this.state.orderForm[key]});
+        }
         let form = (
             <form>
-                <input type="text" name="name" placeholder="Your name" className={classes.Input} />
-                <input type="text" name="email" placeholder="Your email" className={classes.Input} />
-                <input type="text" name="email" placeholder="Your street" className={classes.Input} />
-                <input type="text" name="postal" placeholder="Your postal code" className={classes.Input} />
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                {formElementsArray.map(formElement => (
+                        <Input 
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value} />
+                ))}                
             </form>
         );
         if (this.state.loading) {
@@ -62,8 +98,6 @@ class ContactData extends Component {
             </div>
         );
     }
-
-
 }
 
 export default ContactData;
